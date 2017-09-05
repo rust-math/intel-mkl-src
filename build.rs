44433011040a49df6414110edfa437b1b1034171
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-09-06 02:14:03 +0900

    Add test for lapack link

--- build.rs
+++ build.rs
@@ -38,5 +38,6 @@ fn main() {
     println!("cargo:rustc-link-lib=static=mkl_intel_ilp64");
     println!("cargo:rustc-link-lib=static=mkl_intel_thread");
     println!("cargo:rustc-link-lib=static=iomp5");
+    println!("cargo:rustc-link-lib=static=mkl_core");
     println!("cargo:rustc-link-search=native={}", mkl_dir.display());
 }
