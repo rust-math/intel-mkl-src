c5215628ac3414c85d17882c88136065506313fc
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-09-13 16:57:26 +0900

    Update build.rs

--- build.rs
+++ build.rs
@@ -20,13 +20,16 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-use std::env;
+use std::env::var;
 use std::path::*;
 use std::process::Command;
 use std::fs;
 use std::io::*;
 
-const MKL_ARCHIVE: &'static str = "mkl.tar.xz";
+#[cfg(target_os = "linux")]
+const MKL_ARCHIVE: &'static str = "mkl_linux.tar.xz";
+#[cfg(target_os = "macos")]
+const MKL_ARCHIVE: &'static str = "mkl_osx.tar.xz";
 
 fn download(uri: &str, filename: &str, out_dir: &Path) {
     let out = out_dir.join(filename);
@@ -37,19 +40,46 @@ fn download(uri: &str, filename: &str, out_dir: &Path) {
     f.write(&p.stdout).unwrap();
 }
 
-fn main() {
-    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-    let oid = "7e47b40340b88356058b4cff187ef7598c64658b";
-    let uri = format!(
-        "https://raw.githubusercontent.com/termoshtt/rust-intel-mkl/{}/mkl_lib/mkl.tar.xz",
-        oid
-    );
-    download(&uri, MKL_ARCHIVE, &out_dir);
-    Command::new("tar")
-        .args(&["Jxvf", MKL_ARCHIVE])
+fn check_sum(check_sum_path: &Path, dir: &Path) -> bool {
+    Command::new("md5sum")
+        .args(&["-c", check_sum_path.to_str().unwrap()])
+        .current_dir(dir)
+        .status()
+        .expect("Failed to check md5 sum")
+        .success()
+}
+
+fn expand(archive: &Path, out_dir: &Path) {
+    let st = Command::new("tar")
+        .args(&["xvf", archive.to_str().unwrap()])
         .current_dir(&out_dir)
         .status()
-        .expect("Failed to start decompression (maybe 'tar' is missing?)");
+        .expect("Failed to start expanding archive");
+    if !st.success() {
+        panic!("Failed to expand archive");
+    }
+}
+
+fn main() {
+    let crate_root = PathBuf::from(var("CARGO_MANIFEST_DIR").unwrap());
+    let out_dir = PathBuf::from(var("OUT_DIR").unwrap());
+
+    let oid = "95a818519f0d5dd005aeea7c4b42c929491d75be";
+    let uri = format!(
+        "https://raw.githubusercontent.com/termoshtt/rust-intel-mkl/{}/mkl_lib/{}",
+        oid,
+        MKL_ARCHIVE
+    );
+    let archive_path = out_dir.join(MKL_ARCHIVE);
+    let md5sum_path = crate_root.join(format!("mkl_lib/{}.md5", MKL_ARCHIVE));
+
+    if !archive_path.exists() {
+        download(&uri, MKL_ARCHIVE, &out_dir);
+    }
+    if !check_sum(&md5sum_path, &out_dir) {
+        panic!("check sum of archive is incorrect");
+    }
+    expand(&archive_path, &out_dir);
 
     println!("cargo:rustc-link-search={}", out_dir.display());
     println!("cargo:rustc-link-lib=dylib=mkl_intel_lp64");
