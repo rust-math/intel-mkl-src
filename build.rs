5a1a03e46b76d7f7ed1353345597e8b9412db2f0
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-09-14 11:05:05 +0900

    Calc md5 hash by rust-crypto

--- build.rs
+++ build.rs
@@ -20,6 +20,11 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+extern crate crypto;
+
+use crypto::md5;
+use crypto::digest::Digest;
+
 use std::env::var;
 use std::path::*;
 use std::process::Command;
@@ -27,31 +32,35 @@ use std::fs;
 use std::io::*;
 
 #[cfg(target_os = "linux")]
-const MKL_ARCHIVE: &'static str = "mkl_linux.tar.xz";
+mod mkl {
+    pub const ARCHIVE: &'static str = "mkl_linux.tar.xz";
+    pub const MD5SUM: &'static str = "00c73b0625cac967961e3df255824676";
+    pub const URI: &'static str = "https://www.dropbox.com/s/nnlfdio0ka9yeo1/mkl_linux.tar.xz";
+}
+
 #[cfg(target_os = "macos")]
-const MKL_ARCHIVE: &'static str = "mkl_osx.tar.xz";
+mod mkl {
+    pub const ARCHIVE: &'static str = "mkl_osx.tar.xz";
+    pub const MD5SUM: &'static str = "c44f4fda07fc01863cb06eaf7879bf71";
+    pub const URI: &'static str = "https://www.dropbox.com/s/fw74msh8udjdv28/mkl_osx.tar.xz";
+}
 
 fn download(uri: &str, filename: &str, out_dir: &Path) {
     let out = out_dir.join(filename);
     let mut f = BufWriter::new(fs::File::create(out).unwrap());
-    let p = Command::new("curl").arg(uri).output().expect(
+    let p = Command::new("curl").args(&["-L", uri]).output().expect(
         "Failed to start download",
     );
     f.write(&p.stdout).unwrap();
 }
 
-fn check_sum(check_sum_path: &Path, dir: &Path) -> bool {
-    let cmd = if cfg!(target_os = "linux") {
-        "md5sum"
-    } else {
-        "md5"
-    };
-    Command::new(cmd)
-        .args(&["-c", check_sum_path.to_str().unwrap()])
-        .current_dir(dir)
-        .status()
-        .expect("Failed to check md5 sum")
-        .success()
+fn calc_md5(path: &Path) -> String {
+    let mut sum = md5::Md5::new();
+    let mut f = BufReader::new(fs::File::open(path).unwrap());
+    let mut buf = Vec::new();
+    f.read_to_end(&mut buf).unwrap();
+    sum.input(&buf);
+    sum.result_str()
 }
 
 fn expand(archive: &Path, out_dir: &Path) {
@@ -66,25 +75,20 @@ fn expand(archive: &Path, out_dir: &Path) {
 }
 
 fn main() {
-    let crate_root = PathBuf::from(var("CARGO_MANIFEST_DIR").unwrap());
     let out_dir = PathBuf::from(var("OUT_DIR").unwrap());
+    let archive_path = out_dir.join(mkl::ARCHIVE);
 
-    let oid = "293c05ed4b9bd7bdf61a22f9824dd71e7bbd48f9";
-    let uri = format!(
-        "https://media.githubusercontent.com/media/termoshtt/rust-intel-mkl/{}/mkl_lib/{}",
-        oid,
-        MKL_ARCHIVE
-    );
-    let archive_path = out_dir.join(MKL_ARCHIVE);
-    let md5sum_path = crate_root.join(format!("mkl_lib/{}.md5", MKL_ARCHIVE));
-
-    if archive_path.exists() && check_sum(&md5sum_path, &out_dir) {
+    if archive_path.exists() && calc_md5(&archive_path) == mkl::MD5SUM {
         println!("Use existings archive");
     } else {
         println!("Downlaod archive");
-        download(&uri, MKL_ARCHIVE, &out_dir);
-        if !check_sum(&md5sum_path, &out_dir) {
-            panic!("check sum of downloaded archive is incorrect");
+        download(mkl::URI, mkl::ARCHIVE, &out_dir);
+        let sum = calc_md5(&archive_path);
+        if sum != mkl::MD5SUM {
+            panic!(
+                "check sum of downloaded archive is incorrect: md5sum={}",
+                sum
+            );
         }
     }
     expand(&archive_path, &out_dir);
