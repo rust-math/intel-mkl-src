dab188e9e0de4a7382cb50a81a20c909a7a0eb59
Toshiki Teramura <toshiki.teramura@gmail.com>
2019-11-09 18:48:32 +0900

    Import download fn from intel-mkl-src/build.rs

--- intel-mkl-tool/src/bin/main.rs
+++ intel-mkl-tool/src/bin/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use failure::*;
+use std::{env, path::PathBuf};
+use structopt::StructOpt;
+
+/// CLI tool for intel-mkl crate
+#[derive(Debug, StructOpt)]
+enum Opt {
+    Download { path: PathBuf },
+    PkgConfig {},
+}
+
+fn main() -> Fallible<()> {
+    env::set_var("RUST_LOG", "info");
+    env_logger::init();
+
+    let opt = Opt::from_args();
+
+    match opt {
+        Opt::Download { path } => {
+            intel_mkl_tool::download(&path)?;
+            println!("{:?}", path);
+        }
+        Opt::PkgConfig {} => {
+            unimplemented!();
+        }
+    }
+    Ok(())
 }
--- intel-mkl-tool/src/lib.rs
+++ intel-mkl-tool/src/lib.rs
@@ -1,3 +1,12 @@
+use curl::easy::Easy;
+use failure::*;
+use log::*;
+use std::{
+    fs,
+    io::{self, Write},
+    path::*,
+};
+
 const S3_ADDR: &'static str = "https://s3-ap-northeast-1.amazonaws.com/rust-intel-mkl";
 
 #[cfg(target_os = "linux")]
@@ -19,4 +28,32 @@ mod mkl {
     pub const ARCHIVE: &'static str = "mkl_windows64.tar.xz";
     pub const EXT: &'static str = "lib";
     pub const PREFIX: &'static str = "";
+}
+
+pub fn download(out_dir: &Path) -> Fallible<()> {
+    let archive = out_dir.join(mkl::ARCHIVE);
+    if !archive.exists() {
+        info!("Download archive from AWS S3: {}/{}", S3_ADDR, mkl::ARCHIVE);
+        let f = fs::File::create(&archive)?;
+        let mut buf = io::BufWriter::new(f);
+        let mut easy = Easy::new();
+        easy.url(&format!("{}/{}", S3_ADDR, mkl::ARCHIVE))?;
+        easy.write_function(move |data| Ok(buf.write(data).unwrap()))?;
+        easy.perform()?;
+        assert!(archive.exists());
+    } else {
+        info!("Use existing archive");
+    }
+
+    let core = out_dir.join(format!("{}mkl_core.{}", mkl::PREFIX, mkl::EXT));
+    if !core.exists() {
+        let f = fs::File::open(&archive)?;
+        let de = xz2::read::XzDecoder::new(f);
+        let mut arc = tar::Archive::new(de);
+        arc.unpack(&out_dir)?;
+        assert!(core.exists());
+    } else {
+        info!("Archive has already been extracted");
+    }
+    Ok(())
 }
