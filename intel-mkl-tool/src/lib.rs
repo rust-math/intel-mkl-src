8a98c207e2544856eaedcbf51c1483ff221029cb
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-06-15 15:15:14 +0900

    Update Linux archives, change xdg_home_path

--- intel-mkl-tool/src/lib.rs
+++ intel-mkl-tool/src/lib.rs
@@ -13,16 +13,18 @@ const S3_ADDR: &'static str = "https://s3-ap-northeast-1.amazonaws.com/rust-inte
 
 #[cfg(all(target_os = "linux", target_arch = "x86_64"))]
 mod mkl {
+    pub const OS: &str = "linux";
     pub const ARCHIVE: &'static str = "mkl_linux64";
     pub const EXTENSION_STATIC: &'static str = "a";
     pub const EXTENSION_SHARED: &'static str = "so";
     pub const PREFIX: &'static str = "lib";
-    pub const VERSION_YEAR: u32 = 2019;
-    pub const VERSION_UPDATE: u32 = 5;
+    pub const VERSION_YEAR: u32 = 2020;
+    pub const VERSION_UPDATE: u32 = 1;
 }
 
 #[cfg(all(target_os = "macos", target_arch = "x86_64"))]
 mod mkl {
+    pub const OS: &str = "macos";
     pub const ARCHIVE: &'static str = "mkl_macos64";
     pub const EXTENSION_STATIC: &'static str = "a";
     pub const EXTENSION_SHARED: &'static str = "dylib";
@@ -33,12 +35,23 @@ mod mkl {
 
 #[cfg(all(target_os = "windows", target_arch = "x86_64"))]
 mod mkl {
+    pub const OS: &str = "windows";
     pub const ARCHIVE: &'static str = "mkl_windows64";
     pub const EXTENSION_STATIC: &'static str = "lib";
     pub const EXTENSION_SHARED: &'static str = "lib";
     pub const PREFIX: &'static str = "";
     pub const VERSION_YEAR: u32 = 2019;
     pub const VERSION_UPDATE: u32 = 5;
+}
+
+fn s3_addr() -> String {
+    format!(
+        "{}/{}/{}.{}",
+        S3_ADDR,
+        mkl::OS,
+        mkl::VERSION_YEAR,
+        mkl::VERSION_UPDATE
+    )
 }
 
 pub fn archive_filename() -> String {
@@ -51,7 +64,11 @@ pub fn archive_filename() -> String {
 }
 
 pub fn xdg_home_path() -> PathBuf {
-    dirs::data_local_dir().unwrap().join("intel-mkl-tool")
+    dirs::data_local_dir().unwrap().join(format!(
+        "intel-mkl-tool/{}.{}",
+        mkl::VERSION_YEAR,
+        mkl::VERSION_UPDATE
+    ))
 }
 
 pub fn seek_pkg_config() -> Option<PathBuf> {
