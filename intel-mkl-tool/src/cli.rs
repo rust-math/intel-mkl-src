6778a74f1e8845d12703216b8063397a563db4a1
Toshiki Teramura <toshiki.teramura@gmail.com>
2022-08-02 00:07:56 +0900

    Require path option in intel-mkl-tool download command

--- intel-mkl-tool/src/cli.rs
+++ intel-mkl-tool/src/cli.rs
@@ -11,16 +11,12 @@ enum Opt {
         /// Archive name, e.g. "mkl-static-lp64-iomp". Download all archives if None
         #[structopt(long = "name")]
         name: Option<String>,
-        /// Install destination. Default is `$XDG_DATA_HOME/intel-mkl-tool/${MKL_VERSION}/`
+        /// Install destination
         #[structopt(short = "o", long = "path")]
-        path: Option<PathBuf>,
+        path: PathBuf,
     },
 
     /// Seek Intel-MKL library
-    ///
-    /// 1. pkg-config
-    /// 2. `$XDG_DATA_HOME/intel-mkl-tool`
-    /// will be sought.
     Seek {},
 
     /// Package Intel MKL libraries into an archive
@@ -37,7 +33,6 @@ fn main() -> Result<()> {
 
     match opt {
         Opt::Download { name, path } => {
-            let path = path.unwrap_or(xdg_home_path());
             if let Some(name) = name {
                 let cfg = Config::from_str(&name)?;
                 cfg.download(&path.join(cfg.to_string()))?;
