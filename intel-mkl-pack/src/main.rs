e729ad2f302925acd37e99e7664e99e797dd7db4
Toshiki Teramura <toshiki.teramura@gmail.com>
2022-08-10 04:46:16 +0900

    Create container using intel-mkl-pack

--- intel-mkl-pack/src/main.rs
+++ intel-mkl-pack/src/main.rs
@@ -1,5 +1,29 @@
 mod pack;
 
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use colored::Colorize;
+use intel_mkl_tool::{Config, Library};
+use ocipkg::ImageName;
+use pack::pack;
+use std::time::Instant;
+
+fn main() -> Result<()> {
+    for cfg in Config::possibles() {
+        let lib = Library::new(cfg)?;
+        let (year, _, update) = lib.version()?;
+        let registry = "ghcr.io/rust-math/intel-mkl-src";
+        let name = ImageName::parse(&format!("{}/{}:{}.{}", registry, cfg, year, update))?;
+        let output = format!("{}.tar", cfg);
+
+        eprintln!("{:>12} {}", "Packaging".green().bold(), name);
+        let timer = Instant::now();
+        pack(cfg, &name, &output)?;
+        eprintln!(
+            "{:>12} {} ({:.2}s)",
+            "Created".green().bold(),
+            output,
+            timer.elapsed().as_secs_f32()
+        );
+    }
+    Ok(())
 }
